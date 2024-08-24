47c5266796a47d692a9441fa3feca4ad57f60e39
SFBB <anzhe990206@outlook.com>
2024-08-24 21:42:22 +0800

    Chore: use more standard way to handle error.

--- src/adapter/config.rs
+++ src/adapter/config.rs
@@ -8,21 +8,17 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Result<Self, AdapterError> {
-        let mut err_msg = String::new();
         let mut db_url = String::new();
         match env::var("db_url") {
             Ok(val) => db_url = val.to_string(),
             Err(e) => {
-                err_msg += e.to_string().as_str();
-                err_msg += "\n";
+                print!("Error when creating config: {}", e);
+                return Err(
+                    AdapterError::ConfigLoadFailed
+                )
             }
         }
 
-        if db_url == "" {
-            return Err(
-                AdapterError::ConfigLoadFailed
-            )
-        }
         Ok(Self {
             db_url: db_url,
         })
